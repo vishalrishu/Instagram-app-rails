ff91ea473625f7cb8b8f586b683056ff7f6d731d
Vishal Kumar <vishal.rishu26@gmail.com>
2020-05-18 17:26:58 +0530

    Infinite Scrolling added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,40 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    def index
+        if current_user
+            @posts = Post.paginate(page: params[:page], per_page: 15).order('created_at DESC')
+        else
+          redirect_to new_user_session_path
+        end  
+    end
     def create
         Post.create(post_params)
         redirect_to root_path
     end
 
     def show
-        @post = Post.find(params[:id])
+    end
+    
+    def edit
+    end
+    
+    def update
+        @post.update post_params
+        redirect_to @post
     end
 
     def destroy
         @post = current_user.posts.find(params[:id])
         @post.destroy
 
-        redirect_to user_path(current_user)
+        redirect_to root_path
     end
 
     private
 
+    def set_post
+        @post  = Post.find(params[:id])
+    end
     def post_params
         params.require(:post).permit(:description, :image, :user_id)
     end
